c74deb2a86f20f8890f3b8b1bbc9a9e868ba0a0f
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2023-09-29 02:35:30 -0600

    ....

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddMvc();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 
+//app.UseRouting();
+
 app.MapDefaultControllerRoute();
+
+//app.MapRazorPages();
 
 app.Run();
