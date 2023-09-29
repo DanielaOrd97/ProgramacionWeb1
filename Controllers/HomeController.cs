bb7a625b1f84b9c2a7e33632e61fda16dd9748dd
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2023-09-29 02:09:13 -0600

    ....

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Act1_U1.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Act1_U1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        PerfilViewModel perfil = new PerfilViewModel();
+        public IActionResult Index()
+        {
+            perfil.Materia = "Programación web";
+            perfil.Semestre = 7;
+            perfil.Periodo = "Ago - Dic 2023";
+            perfil.Nombre = "Daniela Gpe.Ordónez Barron";
+            return View(perfil);
+        }
+
+        public IActionResult Perfil()
+        {
+            perfil.Nombre = "Daniela Gpe.Ordónez Barron";
+            perfil.NumControl = "201G0272";
+            perfil.Telefono = "81 1185 5777";
+            perfil.Ciudad = "Nueva Rosita";
+            perfil.Carrera = "Sistemas Computacionales";
+            perfil.Correo = "201G0272@rcarbonifera.tecnm.mx";
+            return View(perfil);
+        }
+    }
+}
---
+++ Models/ViewModels/PerfilViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Act1_U1.Models.ViewModels
+{
+    public class PerfilViewModel
+    {
+        public string Materia { get; set; } = null!;
+        public int Semestre { get; set; }
+        public string Periodo { get; set; } = null!;
+        public string Nombre { get; set; } = null!;
+        public string NumControl { get; set; } = null!;
+        public string Carrera { get; set; } = null!;
+        public string Ciudad { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+        public string?  Correo { get; set; } 
+    }
+}
