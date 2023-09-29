270dd3857379319d772a96abc78453b763c8e9df
Daniela Ordoñez <201G0272@rcarbonifera.tecnm.mx>
2023-09-29 02:23:17 -0600

    ....

---
+++ Act2_Unidad1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Act2_Unidad1.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Act2_Unidad1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(ConversionViewModel cvm, string Tipo)
+        {
+            if (Tipo == "USD")
+            {
+                cvm.Conversion = (cvm.Cantidad * 18).ToString("$ 00.00 MXN");
+                
+            }
+            else if (Tipo == "MXN")
+            {
+                //cvm.Conversion = cvm.Cantidad / 18;
+                cvm.Conversion = (cvm.Cantidad / 18).ToString("$ 00.00 USD");
+            }
+            return View(cvm);
+        }
+    }
+}
---
+++ Act2_Unidad1/Models/ViewModels/ConversionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Act2_Unidad1.Models.ViewModels
+{
+    public class ConversionViewModel
+    {
+        public decimal Cantidad { get; set; }
+        public string Conversion { get; set; } = null!;
+    }
+}
